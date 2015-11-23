77bd57205a5a2b6467d7c13e7014ac4c922e2ff5
erickterri <erick-terri-10@hotmail.com>
2015-11-23 11:22:49 -0600

    base2

--- FG v2/Data/DataSourcePOIData.cs
+++ FG v2/Data/DataSourcePOIData.cs
@@ -14,22 +14,29 @@ namespace Data
         public baseDatos datos = new baseDatos();
         public int iniciarSesion(string correo, string contra)
         {
-            SqlCommand cmd = new SqlCommand();
-            SqlDataAdapter da = new SqlDataAdapter();
-            DataTable dt = new DataTable();
-
-            cmd.Connection = datos.conectarbase();
-            cmd.CommandType = CommandType.Text;
-            cmd.CommandText = "SELECT id from Usuario where correo = '" + correo + "', and contrasenia = '" + contra + "')";
-            da.SelectCommand = cmd;
-            da.Fill(dt);
-            datos.desconectarbase();
-
-            if (dt.Rows.Count > 0)
+            try
             {
-                return Convert.ToInt32(dt.Rows[0][0].ToString());
+                SqlCommand cmd = new SqlCommand();
+                SqlDataAdapter da = new SqlDataAdapter();
+                DataTable dt = new DataTable();
+
+                cmd.Connection = datos.conectarbase();
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = "SELECT id from Usuario where correo = '" + correo + "' and contrasenia = '" + contra + "'";
+                da.SelectCommand = cmd;
+                da.Fill(dt);
+                datos.desconectarbase();
+
+                if (dt.Rows.Count > 0)
+                {
+                    return Convert.ToInt32(dt.Rows[0][0].ToString());
+                }
+                else
+                {
+                    return 0;
+                }
             }
-            else
+            catch (Exception e)
             {
                 return 0;
             }
