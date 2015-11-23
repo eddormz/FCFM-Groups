a467bc7d8dfb250cb4711ab6563a25a0c72d3cd7
erickterri <erick-terri-10@hotmail.com>
2015-11-23 17:22:43 -0600

    sadas

--- FG v2/FG v2/DataSourcePOI.cs
+++ FG v2/FG v2/DataSourcePOI.cs
@@ -57,5 +57,250 @@ namespace FG_v2
             }
         }
 
+
+        public DataTable getPublicacion(int id)
+        {
+            SqlCommand cmd = new SqlCommand();
+            SqlDataAdapter da = new SqlDataAdapter();
+            DataTable dt = new DataTable();
+
+            cmd.Connection = datos.conectarbase();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "SELECT idPublicacion, publicacion, idGrupo, idUsuario FROM Publicacion where idGrupo = " + id;
+            da.SelectCommand = cmd;
+            da.Fill(dt);
+            datos.desconectarbase();
+
+            if (dt.Rows.Count > 0)
+            {
+                return dt;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public bool insertPublicacion(string publicacion, int idGrupo, int idUsuario)
+        {
+            try
+            {
+                SqlCommand cmd = new SqlCommand();
+                SqlDataAdapter da = new SqlDataAdapter();
+                DataTable dt = new DataTable();
+
+                cmd.Connection = datos.conectarbase();
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = "INSERT INTO Publicacion(publicacion,idGrupo,idUsuario)VALUES('" + publicacion + "'," + idGrupo + "," + idUsuario + ")";
+                cmd.ExecuteNonQuery();
+                datos.desconectarbase();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        public bool insertArchivo(string pathArchivo, int idPublicacion, int idMensaje)
+        {
+            try
+            {
+                SqlCommand cmd = new SqlCommand();
+                SqlDataAdapter da = new SqlDataAdapter();
+                DataTable dt = new DataTable();
+
+                cmd.Connection = datos.conectarbase();
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = "INSERT INTO Archivo(pathArchivo,idPublicacion,idMensaje) VALUES('" + pathArchivo + "'," + idPublicacion + "," + idMensaje + ")";
+                cmd.ExecuteNonQuery();
+                datos.desconectarbase();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        public DataTable getArchivosPubli(int id)
+        {
+            SqlCommand cmd = new SqlCommand();
+            SqlDataAdapter da = new SqlDataAdapter();
+            DataTable dt = new DataTable();
+
+            cmd.Connection = datos.conectarbase();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "SELECT idArchivo,pathArchivo,idPublicacion,idMensaje FROM Archivo where idPublicacion = " + id;
+            da.SelectCommand = cmd;
+            da.Fill(dt);
+            datos.desconectarbase();
+
+            if (dt.Rows.Count > 0)
+            {
+                return dt;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+
+        public DataTable getArchivosGrupo(int id)
+        {
+            SqlCommand cmd = new SqlCommand();
+            SqlDataAdapter da = new SqlDataAdapter();
+            DataTable dt = new DataTable();
+
+            cmd.Connection = datos.conectarbase();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "SELECT idArchivo,pathArchivo,idPublicacion,idMensaje FROM Archivo where idMensaje = " + id;
+            da.SelectCommand = cmd;
+            da.Fill(dt);
+            datos.desconectarbase();
+
+            if (dt.Rows.Count > 0)
+            {
+                return dt;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public bool insertSubGrupo(int idUsuario, int idGrupo)
+        {
+            try
+            {
+                SqlCommand cmd = new SqlCommand();
+                SqlDataAdapter da = new SqlDataAdapter();
+                DataTable dt = new DataTable();
+
+                cmd.Connection = datos.conectarbase();
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = "INSERT INTO UsuarioSubGrupo(idUsuario,idGrupo) VALUES(" + idUsuario + "," + idGrupo + ")";
+                cmd.ExecuteNonQuery();
+                datos.desconectarbase();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        public DataTable getSubGrupo(int id)
+        {
+            SqlCommand cmd = new SqlCommand();
+            SqlDataAdapter da = new SqlDataAdapter();
+            DataTable dt = new DataTable();
+
+            cmd.Connection = datos.conectarbase();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "SELECT idUsuarioSubGrupo,idUsuario,idGrupo FROM UsuarioSubGrupo where idUsuario = " + id;
+            da.SelectCommand = cmd;
+            da.Fill(dt);
+            datos.desconectarbase();
+
+            if (dt.Rows.Count > 0)
+            {
+                return dt;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public bool insertTarea(string tarea, int idGrupo)
+        {
+            try
+            {
+                SqlCommand cmd = new SqlCommand();
+                SqlDataAdapter da = new SqlDataAdapter();
+                DataTable dt = new DataTable();
+
+                cmd.Connection = datos.conectarbase();
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = "INSERT INTO Tarea(tarea,status,idGrupo) VALUES ('" + tarea + "', 1, " + idGrupo + ")";
+                cmd.ExecuteNonQuery();
+                datos.desconectarbase();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        public DataTable getTarea(int id)
+        {
+            SqlCommand cmd = new SqlCommand();
+            SqlDataAdapter da = new SqlDataAdapter();
+            DataTable dt = new DataTable();
+
+            cmd.Connection = datos.conectarbase();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "SELECT idTarea,tarea,status,idGrupo FROM Tarea where idGrupo = " + id;
+            da.SelectCommand = cmd;
+            da.Fill(dt);
+            datos.desconectarbase();
+
+            if (dt.Rows.Count > 0)
+            {
+                return dt;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public bool insertComentario(int idPublicacion, string comentario, int idUsuario)
+        {
+            try
+            {
+                SqlCommand cmd = new SqlCommand();
+                SqlDataAdapter da = new SqlDataAdapter();
+                DataTable dt = new DataTable();
+
+                cmd.Connection = datos.conectarbase();
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = "INSERT INTO Comentario(idPublicacion,comentario,idUsuario) VALUES (" + idPublicacion + ", '" + comentario + "', " + idUsuario + " )";
+                cmd.ExecuteNonQuery();
+                datos.desconectarbase();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        public DataTable getComentario(int id)
+        {
+            SqlCommand cmd = new SqlCommand();
+            SqlDataAdapter da = new SqlDataAdapter();
+            DataTable dt = new DataTable();
+
+            cmd.Connection = datos.conectarbase();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "SELECT idComentario,idPublicacion,comentario,idUsuario FROM Comentario where idPublicacion = " + id;
+            da.SelectCommand = cmd;
+            da.Fill(dt);
+            datos.desconectarbase();
+
+            if (dt.Rows.Count > 0)
+            {
+                return dt;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
     }
 }
